463d00e85ad359658903a204fef778e8e9561149
abhishek johri <johriabhishek24@gmail.com>
2024-02-05 17:44:32 +0530

    'd'

--- src/pages/LiveMap/TripHistory/MapLine.js
+++ src/pages/LiveMap/TripHistory/MapLine.js
@@ -1,131 +1,130 @@
-
-import React, { useState, useEffect,useRef } from 'react'
-import { GoogleMap, useJsApiLoader, Circle, Marker,InfoWindow,Polyline,DirectionsService, DirectionsRenderer  } from '@react-google-maps/api';
+import React, { useState, useEffect, useRef } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Circle,
+  Marker,
+  InfoWindow,
+  Polyline,
+  DirectionsService,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 // import { CarOutlined } from '@ant-design/icons';
-import { CarOutlined } from '@ant-design/icons';
-import DirectionsCarFilledIcon from '@mui/icons-material/DirectionsCarFilled';
-import moment from 'moment';
-import MarkerItem2 from '../MapItems/MarkerItem2';
+import { CarOutlined } from "@ant-design/icons";
+import DirectionsCarFilledIcon from "@mui/icons-material/DirectionsCarFilled";
+import moment from "moment";
+import MarkerItem2 from "../MapItems/MarkerItem2";
 
 const containerStyle = {
-  width: '100%',
-  height: '100%'
+  width: "100%",
+  height: "100%",
 };
 
-export default function MapLine({aniActive,item,distance,setDistance}) {
-    
-      const [selectedMarker, setSelectedMarker] = useState(null);
-    
-    //   const toggleInfoWindow = (markerId) => {
-    //     console.log(markerId)
-    //     if (selectedMarker === markerId) {
-    //       // If the same marker is clicked again, close its InfoWindow
-    //       setSelectedMarker(null);
-    //     } else {
-    //       // Otherwise, open the InfoWindow for the clicked marker
-    //       setSelectedMarker(markerId);
-    //     }
-    //   };
-    
+export default function MapLine({ aniActive, item, distance, setDistance }) {
+  const [selectedMarker, setSelectedMarker] = useState(null);
 
-      const [directions, setDirections] = useState(null);
-      const [map, setMap] = useState(null);
-      const [path, setPath] = useState([]);
+  //   const toggleInfoWindow = (markerId) => {
+  //     console.log(markerId)
+  //     if (selectedMarker === markerId) {
+  //       // If the same marker is clicked again, close its InfoWindow
+  //       setSelectedMarker(null);
+  //     } else {
+  //       // Otherwise, open the InfoWindow for the clicked marker
+  //       setSelectedMarker(markerId);
+  //     }
+  //   };
+
+  const [directions, setDirections] = useState(null);
+  const [map, setMap] = useState(null);
+  const [path, setPath] = useState([]);
   const [pathIndex, setPathIndex] = useState(0);
-    
 
-   
+  useEffect(() => {
+    const directionsService = new window.google.maps.DirectionsService();
+    let a = item.length;
+    console.log(a);
+    console.log(item?.data[a - 2]);
+    const origin = {
+      lat: parseFloat(item?.data[0]?.lat),
+      lng: parseFloat(item?.data[0]?.lng),
+    }; // Replace with your start coordinates
+    const destination = {
+      lat: parseFloat(item?.data[item?.data.length - 1]?.lat),
+      lng: parseFloat(item?.data[item?.data.length - 1]?.lng),
+    };
+    console.log(origin, destination);
+    directionsService.route(
+      {
+        origin,
+        destination,
+        travelMode: "DRIVING", // You can change this to other travel modes like 'WALKING' or 'BICYCLING'
+      },
+      (result, status) => {
+        if (status === "OK") {
+          setDirections(result);
 
-    useEffect(() => {
-        const directionsService = new window.google.maps.DirectionsService();
-        let a = item.length
-        console.log(a)
-        console.log(item?.data[a-2])
-        const origin = { lat: parseFloat(item?.data[0]?.lat), lng: parseFloat(item?.data[0]?.lng) }; // Replace with your start coordinates
-        const destination = { lat: parseFloat(item?.data[item?.data.length-1]?.lat), lng: parseFloat(item?.data[item?.data.length-1]?.lng) };
-        console.log(origin,destination)
-        directionsService.route(
-          {
-            origin,
-            destination,
-            travelMode: 'DRIVING', // You can change this to other travel modes like 'WALKING' or 'BICYCLING'
-          },
-          (result, status) => {
-            if (status === 'OK') {
-              setDirections(result);
-              
-              const routePath = result.routes[0].overview_path;
-              // console.log(routePath,"routepath")
-              setPath(routePath);
-              const route = result.routes[0].legs[0];
-              const distanceInMeters = route.distance.value;
-              console.log(distanceInMeters,"distance meters")
-              setDistance(distanceInMeters);
-            } else {
-              console.error(`Directions request failed: ${status}`);
-            }
-          }
-        );
-      }, [item]);
+          const routePath = result.routes[0].overview_path;
+          // console.log(routePath,"routepath")
+          setPath(routePath);
+          const route = result.routes[0].legs[0];
+          const distanceInMeters = route.distance.value;
+          console.log(distanceInMeters, "distance meters");
+          setDistance(distanceInMeters);
+        } else {
+          console.error(`Directions request failed: ${status}`);
+        }
+      }
+    );
+  }, [item]);
 
-
-      useEffect(() => {
-        if(aniActive){
-        const interval = setInterval(() => {
-          if (pathIndex < path.length) {
-            setPathIndex(pathIndex + 1);
-          } else {
-            clearInterval(interval);
-          }
-        }, 100); // Adjust the interval to control the drawing speed (e.g., 100ms for slower motion)
-        return () => clearInterval(interval);
+  useEffect(() => {
+    if (aniActive) {
+      const interval = setInterval(() => {
+        if (pathIndex < path.length) {
+          setPathIndex(pathIndex + 1);
+        } else {
+          clearInterval(interval);
+        }
+      }, 100); // Adjust the interval to control the drawing speed (e.g., 100ms for slower motion)
+      return () => clearInterval(interval);
     }
-      }, [path, pathIndex,aniActive]);
-    // console.log(directions,"dirrr****************")
-
+  }, [path, pathIndex, aniActive]);
+  // console.log(directions,"dirrr****************")
 
   return (
     <div>
-
-
-
-{directions && (
-    <>
-        <DirectionsRenderer
-          directions={directions}
-          options={{ suppressMarkers: true }}
-          
-        />
-        <Polyline
-
-        path={path.slice(0, pathIndex)}
-        options={{
-          strokeColor: 'red', // Color of the line
-          strokeOpacity: 1, // Opacity of the line
-          strokeWeight: 4, // Thickness of the line
-        }}
-      />
-      </> 
+      {directions && (
+        <>
+          <DirectionsRenderer
+            directions={directions}
+            options={{ suppressMarkers: true }}
+          />
+          <Polyline
+            path={path.slice(0, pathIndex)}
+            options={{
+              strokeColor: "red", // Color of the line
+              strokeOpacity: 1, // Opacity of the line
+              strokeWeight: 4, // Thickness of the line
+            }}
+          />
+        </>
       )}
 
-        {selectedMarker === item?.latestDocument?._id &&  
+      {selectedMarker === item?.latestDocument?._id && (
         <InfoWindow
-            key={item.id}
-            position={{
-              lat: parseFloat(item?.latestDocument?.lat),
-              
-              lng: parseFloat(item?.latestDocument?.lng),
-            }}
-            onCloseClick={() => setSelectedMarker(null)}
-            options={{ maxWidth: 200 }}
-            visible={selectedMarker === item.id}
-          >
-           
-         <MarkerItem2 item={item} />
-          </InfoWindow>
-}
+          key={item.id}
+          position={{
+            lat: parseFloat(item?.latestDocument?.lat),
 
-
+            lng: parseFloat(item?.latestDocument?.lng),
+          }}
+          onCloseClick={() => setSelectedMarker(null)}
+          options={{ maxWidth: 200 }}
+          visible={selectedMarker === item.id}
+        >
+          <MarkerItem2 item={item} />
+        </InfoWindow>
+      )}
     </div>
-  )
+  );
 }
