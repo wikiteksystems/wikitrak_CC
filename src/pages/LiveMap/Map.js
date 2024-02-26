d821114a1f90e56864802991d5a3ca96c925d1ad
tilak-atpl <tilakatpl@gmail.com>
2024-02-26 15:02:40 +0530

    resolved dashboard issue

--- src/pages/LiveMap/Map.js
+++ src/pages/LiveMap/Map.js
@@ -22,6 +22,7 @@ import {
 import { CarOutlined } from "@ant-design/icons";
 import DirectionsCarFilledIcon from "@mui/icons-material/DirectionsCarFilled";
 import MarkerItem1 from "./MapItems/MarkerItem1";
+import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
 import moment from "moment";
 import WeatherForecast from "./WeatherForecast";
 import MarkerItem2 from "./MapItems/MarkerItem2";
@@ -228,7 +229,7 @@ function Map({
 
   //   return positiveAngle;
   // };
-  const calculateAngleFromEast = (heading) => {
+  const calculateAngleFromEast = (heading, imei) => {
     console.log(heading, 'heading')
     // Assuming heading is in degrees
     // Convert heading to radians
@@ -239,7 +240,7 @@ function Map({
     // Convert angle to degrees
     const angleDegrees = (angleFromNorthClockwise * 180) / Math.PI;
     // Return the angle in degrees
-    console.log(angleDegrees , "angleDegrees..")
+    console.log(angleDegrees,imei , "angleDegrees, imei")
     return angleDegrees;
 };
 
@@ -535,7 +536,6 @@ function Map({
                   // rotation: item?.latestDocument?.heading,
                   // rotation: (item?.latestDocument?.heading * 180) / Math.PI,
                   rotation: 90,
-                  anchor: new google.maps.Point(16, 16),
                   scale: 1,
                 }}
               >
@@ -674,7 +674,7 @@ function Map({
                   // rotation: item?.latestDocument?.heading,
                   // rotation: (item?.latestDocument?.heading * 180) / Math.PI,
                   rotation: calculateAngleFromEast(
-                    item?.latestDocument?.heading
+                    item?.latestDocument?.heading, item?.latestDocument?.imei
                   ),
                   scale: 1,
                 }}
@@ -718,6 +718,7 @@ function Map({
                                     // animation="BOUNCE"
                                     icon={{
                                         path: "M29.395,0H17.636c-3.117,0-5.643,3.467-5.643,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759c3.116,0,5.644-2.527,5.644-5.644V6.584C35.037,3.467,32.511,0,29.395,0z M34.05,14.188v11.665l-2.729,0.351v-4.806L34.05,14.188z M32.618,10.773c-1.016,3.9-2.219,8.51-2.219,8.51H16.631l-2.222-8.51C14.41,10.773,23.293,7.755,32.618,10.773z M15.741,21.713v4.492l-2.73-0.349V14.502L15.741,21.713z M13.011,37.938V27.579l2.73,0.343v8.196L13.011,37.938z M14.568,40.882l2.218-3.336h13.771l2.219,3.336H14.568z M31.321,35.805v-7.872l2.729-0.355v10.048L31.321,35.805",
+                                        // path: "iconUrl",
                                         fillColor: apiData?.imeiList.includes(
                                             parseFloat(item.latestDocument.imei)
                                         )
@@ -725,7 +726,7 @@ function Map({
                                             : "grey",
                                         fillOpacity: 2,
                                         strokeWeight: 1,
-                                        rotation: calculateAngleFromEast(item?.latestDocument?.heading),
+                                        rotation: calculateAngleFromEast(item?.latestDocument?.heading, item?.latestDocument?.imei),
                                         scale: 1,
                                     }}
                                 >
