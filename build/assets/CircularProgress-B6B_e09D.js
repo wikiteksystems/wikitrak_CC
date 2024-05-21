7160236fea6f45b6d8f083edf05f69498979c830
tilak-atpl <tilak.ram@autopeepal.com>
2024-05-21 12:37:44 +0530

    apploading acre calculation

---
+++ build/assets/CircularProgress-B6B_e09D.js
@@ -0,0 +1,28 @@
+import{a$ as E,b0 as T,r as B,aX as U,C as F,D,j as h,w as d,E as R,L as j,b1 as z,a_ as G,aU as K,J as L,ac as I,M as k,O as f,ad as w,N as W,I as H}from"./index-CdPlqq14.js";const J=["className","component"];function O(e={}){const{themeId:r,defaultTheme:s,defaultClassName:a="MuiBox-root",generateClassName:n}=e,m=E("div",{shouldForwardProp:t=>t!=="theme"&&t!=="sx"&&t!=="as"})(T);return B.forwardRef(function(g,c){const i=U(s),u=F(g),{className:v,component:l="div"}=u,x=D(u,J);return h.jsx(m,d({as:l,ref:c,className:R(v,n?n(a):a),theme:r&&i[r]||i},x))})}const V=j("MuiBox",["root"]),X=V,Z=z(),q=O({themeId:G,defaultTheme:Z,defaultClassName:X.root,generateClassName:K.generate}),ne=q;function A(e){return L("MuiCircularProgress",e)}j("MuiCircularProgress",["root","determinate","indeterminate","colorPrimary","colorSecondary","svg","circle","circleDeterminate","circleIndeterminate","circleDisableShrink"]);const Q=["className","color","disableShrink","size","style","thickness","value","variant"];let p=e=>e,S,N,M,_;const o=44,Y=I(S||(S=p`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+`)),ee=I(N||(N=p`
+  0% {
+    stroke-dasharray: 1px, 200px;
+    stroke-dashoffset: 0;
+  }
+
+  50% {
+    stroke-dasharray: 100px, 200px;
+    stroke-dashoffset: -15px;
+  }
+
+  100% {
+    stroke-dasharray: 100px, 200px;
+    stroke-dashoffset: -125px;
+  }
+`)),re=e=>{const{classes:r,variant:s,color:a,disableShrink:n}=e,m={root:["root",s,`color${f(a)}`],svg:["svg"],circle:["circle",`circle${f(s)}`,n&&"circleDisableShrink"]};return H(m,A,r)},se=k("span",{name:"MuiCircularProgress",slot:"Root",overridesResolver:(e,r)=>{const{ownerState:s}=e;return[r.root,r[s.variant],r[`color${f(s.color)}`]]}})(({ownerState:e,theme:r})=>d({display:"inline-block"},e.variant==="determinate"&&{transition:r.transitions.create("transform")},e.color!=="inherit"&&{color:(r.vars||r).palette[e.color].main}),({ownerState:e})=>e.variant==="indeterminate"&&w(M||(M=p`
+      animation: ${0} 1.4s linear infinite;
+    `),Y)),ae=k("svg",{name:"MuiCircularProgress",slot:"Svg",overridesResolver:(e,r)=>r.svg})({display:"block"}),te=k("circle",{name:"MuiCircularProgress",slot:"Circle",overridesResolver:(e,r)=>{const{ownerState:s}=e;return[r.circle,r[`circle${f(s.variant)}`],s.disableShrink&&r.circleDisableShrink]}})(({ownerState:e,theme:r})=>d({stroke:"currentColor"},e.variant==="determinate"&&{transition:r.transitions.create("stroke-dashoffset")},e.variant==="indeterminate"&&{strokeDasharray:"80px, 200px",strokeDashoffset:0}),({ownerState:e})=>e.variant==="indeterminate"&&!e.disableShrink&&w(_||(_=p`
+      animation: ${0} 1.4s ease-in-out infinite;
+    `),ee)),oe=B.forwardRef(function(r,s){const a=W({props:r,name:"MuiCircularProgress"}),{className:n,color:m="primary",disableShrink:y=!1,size:t=40,style:g,thickness:c=3.6,value:i=0,variant:u="indeterminate"}=a,v=D(a,Q),l=d({},a,{color:m,disableShrink:y,size:t,thickness:c,value:i,variant:u}),x=re(l),C={},P={},$={};if(u==="determinate"){const b=2*Math.PI*((o-c)/2);C.strokeDasharray=b.toFixed(3),$["aria-valuenow"]=Math.round(i),C.strokeDashoffset=`${((100-i)/100*b).toFixed(3)}px`,P.transform="rotate(-90deg)"}return h.jsx(se,d({className:R(x.root,n),style:d({width:t,height:t},P,g),ownerState:l,ref:s,role:"progressbar"},$,v,{children:h.jsx(ae,{className:x.svg,ownerState:l,viewBox:`${o/2} ${o/2} ${o} ${o}`,children:h.jsx(te,{className:x.circle,style:C,ownerState:l,cx:o,cy:o,r:(o-c)/2,fill:"none",strokeWidth:c})})}))}),ce=oe;export{ne as B,ce as C};
