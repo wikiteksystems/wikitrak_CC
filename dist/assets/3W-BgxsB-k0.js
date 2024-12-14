2ad0e935cbd5f9f349802db3a9cf8f034f0edcf5
tilak-atpl <tilak.ram@autopeepal.com>
2024-12-14 19:52:35 +0530

    updating new version

--- dist/assets/3W-BgxsB-k0.js
+++
@@ -1,1 +0,0 @@
-const e="/assets/4W-zFlpw5ld.png",s="/assets/motorbike-Ve1wpD8A.png",t="/assets/3W-DS7zfQvi.png";export{e as f,s as m,t};
---
+++ dist/assets/preet_tractor-CDwt_mVh.js
@@ -0,0 +1,1 @@
+const s="/assets/4W-zFlpw5ld.png",e="/assets/motorbike-Ve1wpD8A.png",t="/assets/3W-DS7zfQvi.png",r="/assets/preet_tractor-2erNEHvu.png";export{t as a,s as f,e as m,r as t};
