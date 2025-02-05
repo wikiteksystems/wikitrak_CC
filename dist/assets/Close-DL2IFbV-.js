53b628d950dc518a2ed564e9efa190a13c162c20
aniketAutopeepal <aniket.mhaske@autopeepal.com>
2025-02-05 12:02:29 +0530

    new build 5 feb

--- dist/assets/Close-DL2IFbV-.js
+++
@@ -1,1 +0,0 @@
-import{a6 as r,j as t}from"./index-BSYSVMk_.js";import{r as a}from"./createSvgIcon-Cq6neHNC.js";var e={},o=r;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,s=o(a()),i=t;u=e.default=(0,s.default)((0,i.jsx)("path",{d:"M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}),"Close");export{u as d};
---
+++ dist/assets/NewLoader-XCUTlytE.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-jaVnQRPi.js";import{C as r}from"./menu2-OL55J3AA.js";const t=()=>e.jsx("div",{style:{position:"fixed",top:0,left:0,width:"100%",height:"100%",backgroundColor:"rgba(0, 0, 0, 0.5)",display:"flex",justifyContent:"center",alignItems:"center",zIndex:1050},children:e.jsx(r,{})}),i=t;export{i as N};
