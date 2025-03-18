cb5d44d15fb562c9a6ff527fa744e21745bd1eed
aniketAutopeepal <aniket.mhaske@autopeepal.com>
2025-03-18 09:38:07 +0530

    new build 18march 0938

--- dist/assets/Add-B1S9LoUg.js
+++
@@ -1,1 +0,0 @@
-import{a4 as r,j as t}from"./index-C9JU8Tc5.js";import{r as a}from"./createSvgIcon-edK-DtHh.js";var e={},o=r;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,d=o(a()),v=t;u=e.default=(0,d.default)((0,v.jsx)("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"}),"Add");export{u as d};
