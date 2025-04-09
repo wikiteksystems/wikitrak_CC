b4e19c9fa9eabbf8259f60db7bee7c93109d3e4a
aniketAutopeepal <aniket.mhaske@autopeepal.com>
2025-04-09 18:07:59 +0530

    new latest build 9 april

---
+++ dist/assets/Add-p7-2OPjY.js
@@ -0,0 +1,1 @@
+import{a3 as r,j as t}from"./index-BSVJbJcX.js";import{r as a}from"./createSvgIcon-C-6S6yhG.js";var e={},o=r;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,d=o(a()),v=t;u=e.default=(0,d.default)((0,v.jsx)("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"}),"Add");export{u as d};
