eab435ed7baec62bc23fcfa6fa913fff88cc7b88
aniketAutopeepal <aniket.mhaske@autopeepal.com>
2025-04-15 17:40:45 +0530

    new build 15 april 1740

---
+++ dist/assets/Add-CxHSCGUN.js
@@ -0,0 +1,1 @@
+import{aa as r,j as a}from"./index-BmWIDii2.js";import{r as t}from"./createSvgIcon-Jgvla9HZ.js";var e={},o=r;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,d=o(t()),v=a;u=e.default=(0,d.default)((0,v.jsx)("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"}),"Add");export{u as d};
