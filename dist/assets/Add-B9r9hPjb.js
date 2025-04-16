5909ec60fa53493a5c7c5f1dd528b76bf80214be
aniketAutopeepal <aniket.mhaske@autopeepal.com>
2025-04-16 14:17:13 +0530

    new build  wed 16 april

---
+++ dist/assets/Add-B9r9hPjb.js
@@ -0,0 +1,1 @@
+import{aa as r,j as a}from"./index-pD_d63wZ.js";import{r as t}from"./createSvgIcon-CG4pyggZ.js";var e={},o=r;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,d=o(t()),v=a;u=e.default=(0,d.default)((0,v.jsx)("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"}),"Add");export{u as d};
---
+++ dist/assets/NewLoader-2N10PELv.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-pD_d63wZ.js";import{C as r}from"./CircularProgress-Chb3pEA7.js";const t=()=>e.jsx("div",{style:{position:"fixed",top:0,left:0,width:"100%",height:"100%",backgroundColor:"rgba(0, 0, 0, 0.5)",display:"flex",justifyContent:"center",alignItems:"center",zIndex:1050},children:e.jsx(r,{})}),i=t;export{i as N};
