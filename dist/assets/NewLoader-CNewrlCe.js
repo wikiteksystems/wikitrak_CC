c4841e16417b24d969abdaf92ab40e8bf0f5ca4e
aniketAutopeepal <aniket.mhaske@autopeepal.com>
2025-07-09 14:56:55 +0530

    new build jul 9  1456

---
+++ dist/assets/NewLoader-CNewrlCe.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Cjcv2yiw.js";import{C as r}from"./CircularProgress-Cn0algju.js";const t=()=>e.jsx("div",{style:{position:"fixed",top:0,left:0,width:"100%",height:"100%",backgroundColor:"rgba(0, 0, 0, 0.5)",display:"flex",justifyContent:"center",alignItems:"center",zIndex:1050},children:e.jsx(r,{})}),i=t;export{i as N};
---
+++ dist/assets/usePreviousProps-6n5ImV6D.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index-Cjcv2yiw.js";const o=s=>{const r=e.useRef({});return e.useEffect(()=>{r.current=s}),r.current};export{o as u};
